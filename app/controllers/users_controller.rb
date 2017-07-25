80f940a51ac7a891ae193cd5df1b7b9679c08ab6
Sunjay <lorenzianheteroceran@gmail.com>
2017-07-25 17:06:41 -0500

    Registry controllers but no tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,34 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(
+			{
+				fullname: params[:user][:fullname],
+				email: params[:user][:email],
+				password: params[:user][:password]
+			}
+		)
+		# Checks if there's already a user with that email
+		if User.find_by(email: @user.email)
+			@errors = ["There already is a user with #{@user.email}"]
+		else
+			# Checks for matching passwords
+			if params[:user][:password] == params[:user][:password_confirmation]
+				# Checks for valid properties
+				if @user.save
+					redirect reports_new_path
+				else
+					@errors = @user.errors.full_messages
+				end
+			else
+				@errors = ['Passwords do not match']
+			end
+		end
+		render :new
 	end
 
 end
